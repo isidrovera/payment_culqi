4ce4a58353e45ffb9c1258caf21c51da8a7ca072
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-06-05 12:23:35 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,26 @@
-# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Payment Provider: Culqi',
     'version': '1.0',
     'category': 'Accounting/Payment Providers',
     'sequence': 351,
-    'summary': "Proveedor de pago Culqi para Perú",
-    'description': " ",  # No debe estar vacío para evitar cargar README.md
+    'summary': "Culqi Payment Integration for Peru: tarjetas, Yape, PagoEfectivo y más",
+    'description': "Integración de Culqi con Odoo 18 para pagos con tarjeta, billeteras, QR y más.",
     'depends': ['payment'],
     'data': [
+        'views/payment_form_templates.xml',
         'views/payment_provider_views.xml',
-        'views/payment_culqi_templates.xml',
+        'views/payment_transaction_views.xml',
         'data/payment_provider_data.xml',
     ],
-    'post_init_hook': 'post_init_hook',  # 🔁 Aquí se invoca el hook al instalar
+    'post_init_hook': 'post_init_hook',
     'assets': {
         'web.assets_frontend': [
             'payment_culqi/static/src/**/*',
         ],
     },
     'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
 }
