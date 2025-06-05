286f929c9d6a8d1e19e51a132dd62ed11d40efcd
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-06-05 12:28:27 -0500

    Update hooks.py

--- hooks.py
+++ hooks.py
@@ -1,16 +1,14 @@
-# payment_culqi/hooks.py
-
 def post_init_hook(cr, registry):
     from odoo.api import Environment
     env = Environment(cr, 1, {})
 
     culqi_provider = env['payment.provider'].search([('code', '=', 'culqi')], limit=1)
-    if not culqi_provider:
-        return
-
-    env['payment.method'].create({
-        'name': 'Culqi (Tarjeta)',
-        'code': 'culqi_card',
-        'provider_id': culqi_provider.id,
-        'available_on': ['portal'],
-    })
+    if culqi_provider:
+        env['payment.method'].create({
+            'name': 'Culqi (Tarjeta)',
+            'code': 'culqi_card',
+            'provider_id': culqi_provider.id,
+            'available_on': ['portal'],
+            'payment_flow': 'direct',
+            'state': 'enabled',
+        })
